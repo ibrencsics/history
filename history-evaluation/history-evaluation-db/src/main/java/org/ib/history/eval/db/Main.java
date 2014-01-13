4f378c90e93f8c9f1a4a98a49b0c8ce29ffc1910
Ivan Brencsics <ivan.brencsics@gmail.com>
2014-01-13 23:11:38 +0100

    initial commit

---
+++ history-evaluation/history-evaluation-db/src/main/java/org/ib/history/eval/db/Main.java
@@ -0,0 +1,11 @@
+package org.ib.history.eval.db;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ivan
+ * Date: 1/13/14
+ * Time: 10:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
