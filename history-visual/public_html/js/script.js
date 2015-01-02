cad4191d80a5bb502560b707b6e478ddc79edb94
Ivan Brencsics <ivan.brencsics@gmail.com>
2015-01-02 15:24:14 +0100

    D3IA ch3

--- history-visual/public_html/js/script.js
+++ history-visual/public_html/js/script.js
@@ -2,12 +2,6 @@ function doTheThing(){
     alert('This is the thing!');
 }
 
-var elem = document.getElementById("about");
-elem.addEventListener('click', doTheThing, false);
-
-var elem2 = document.getElementById("copy");
-elem2.addEventListener('click', doTheThing, false);
-
 var elem3 = document.getElementById("chart1");
 elem3.addEventListener('click', case0410.show, false);
 
@@ -16,3 +10,6 @@ elem4.addEventListener('click', case0420.show, false);
 
 var elem5 = document.getElementById("family-tree");
 elem5.addEventListener('click', familyTree.show, false)
+
+var elem6 = document.getElementById("E1");
+elem6.addEventListener('click', englishKings.show, false)
