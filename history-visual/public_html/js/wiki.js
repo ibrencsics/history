149cfb7291411e5cb2a4484fac337c3cd50a8c13
Ivan Brencsics <ivan.brencsics@gmail.com>
2015-02-01 00:01:06 +0100

    Javascript code improved

--- history-visual/public_html/js/wiki.js
+++ history-visual/public_html/js/wiki.js
@@ -7,8 +7,6 @@ var wiki = {
         d3.select("#data").selectAll("*").remove();
         
         wiki.showControls();
-        
-        //wiki.query("William_the_Conqueror");
     },
     
     showControls : function() {
@@ -24,13 +22,22 @@ var wiki = {
         d3.text("wiki-modal-details.html", function(data) {
             d3.select("#data").append("div").attr("id", "details").html(data);
         });
+        
+        d3.text("node-contextmenu.html", function(data) {
+            d3.select("#details").append("div").attr("id", "node-contextmenu").html(data);
+        });
+        
+//        d3.select("svg")
+//                .on("contextmenu", function(d,i) {
+//                    showBaseContextMenu(this, d, i);
+////                    d3.event.preventDefault();
+//                })
     },
     
     buttonClick : function() {
         d3.select("#dropdown").selectAll("*").remove();
         
         wikiPage = document.getElementById("wikiPage").value;
-        console.log(wikiPage);
         
         d3.select("svg").selectAll("*").remove();
         wiki.query1(wikiPage);
@@ -71,19 +78,7 @@ var wiki = {
     // visualization
     
     dataViz : function(nodes, edges) {
-        var nodeHash = {};
-        for (x in nodes) {
-            nodeHash[nodes[x].id] = nodes[x];
-        }
-        for (x in edges) {
-            if (edges[x].type == "IS_SPOUSE_OF") {
-                edges[x].weight = 5;
-            } else {
-                edges[x].weight = 1;
-            }
-            edges[x].source = nodeHash[edges[x].source];
-            edges[x].target = nodeHash[edges[x].target];
-        }
+        populateEdges(nodes, edges);
         
         var weightScale = d3.scale.linear().domain(d3.extent(edges, function(d) {return d.weight})).range([.1,1])
         
@@ -93,84 +88,101 @@ var wiki = {
             .gravity(.3)
             .linkDistance(50)
             .linkStrength(function (d) {return weightScale(d.weight)})
-            .size([500,500]).nodes(nodes)
-            .links(edges).on("tick", forceTick);
+            .size([500,500])
+            .nodes(nodes)
+            .links(edges)
+            .on("tick", forceTick);
 
         showGraph(nodes, edges);
       
-        d3.select("#dropdown")
-                .selectAll("option")
-                .data(nodes)
-                .enter()
-                .append("option")
-                .text(function (d) {return d.id})
-      
-        d3.text("wiki-contextmenu.html", function(data) {
-            d3.select("#details").append("div").attr("id", "contextmenu").html(data);
-        });
-        
-//      d3.selectAll("line").attr("marker-end", "url(#Triangle)");
-        
         force.start();
 
 
         // functions
 
+        function populateEdges(nodes, edges) {
+            nodeHash = {};
+            for (x in nodes) {
+                nodeHash[nodes[x].id] = nodes[x];
+            }
+            for (x in edges) {
+                if (edges[x].type == "IS_SPOUSE_OF") {
+                    edges[x].weight = 5;
+                } else {
+                    edges[x].weight = 1;
+                }
+                edges[x].source = nodeHash[edges[x].source];
+                edges[x].target = nodeHash[edges[x].target];
+            }
+        }
+
         // show graph
 
         function showGraph(nodes, edges) {
-            d3.select("svg").selectAll("line.link").data(edges, function (d) {return d.source.id + "-" + d.target.id}).enter()
-                .append("line")
-                .attr("class", "link")
-                .style("stroke", "black")
-                .style("opacity", .5)
-                .style("stroke-width", function(d) {return d.weight})
+            link = d3.select("svg")
+                    .selectAll("line.link")
+                    .data(edges, function (d) {return d.source.id + "-" + d.target.id});
+            
+            link.enter()
+                    .insert("line", ".node")
+                    .attr("class", "link")
+                    .style("stroke", "black")
+                    .style("opacity", .5)
+                    .style("stroke-width", function(d) {return d.weight})
+            link.exit().remove;
 
-            var nodeEnter = d3.select("svg").selectAll("g.node").data(nodes, function (d) {return d.id}).enter()
-                .append("g")
-                .attr("class", "node")
-                .call(force.drag())
-                .on("click", fixNode)
-                .on("dblclick", releaseNode)
-                .on("contextmenu", function(d,i) {
-                    showContextMenu(this, d, i);
-                    d3.event.preventDefault();
-                })
-                .on("mouseover", function() {
-                    d3.select(this)
-                        .style("stroke", "black")
-                        .style("stroke-width", "1px")
-                        .style("cursor", "hand")
-                })
-                .on("mouseleave", function() {
-                    d3.select(this)
-                        .style("stroke", "black")
-                        .style("stroke-width", "0px")
-                        .style("cursor", "arrow")
-                })
+            node = d3.select("svg")
+                    .selectAll("g.node")
+                    .data(nodes, function (d) {return d.id});
+            
+            node.enter()
+                    .append("g")
+                    .attr("class", "node")
+                    .call(force.drag())
+                    .on("click", fixNode)
+                    .on("dblclick", releaseNode)
+                    .on("contextmenu", function(d,i) {
+                        showNodeContextMenu(this, d, i);
+                        d3.event.preventDefault();
+                    })
+                    .on("mouseover", function() {
+                        d3.select(this)
+                            .style("stroke", "black")
+                            .style("stroke-width", "1px")
+                            .style("cursor", "hand")
+                    })
+                    .on("mouseleave", function() {
+                        d3.select(this)
+                            .style("stroke", "black")
+                            .style("stroke-width", "0px")
+                            .style("cursor", "arrow")
+                    })
 
-            nodeEnter.append("circle")
-                .attr("r", 12)
-                .style("fill", function(d) {
-                    gender = d.gender;
-                    if (gender == "MALE") return "BLUE";
-                    else if (gender == "FEMALE") return "RED";
-                    else return "GREY";
-                })
-                .style("stroke", "black")
-                .style("stroke-width", "1px")
+            node.append("circle")
+                    .attr("r", 12)
+                    .style("fill", function(d) {
+                        gender = d.gender;
+                        if (gender == "MALE") return "BLUE";
+                        else if (gender == "FEMALE") return "RED";
+                        else return "GREY";
+                    })
+                    .style("stroke", "black")
+                    .style("stroke-width", "1px")
 
-            nodeEnter.append("text")
-                .style("text-anchor", "middle")
-                .style("font-size", "75%")
-                .attr("y", 20)
-                .text(function(d) {return d.name})
+            node.append("text")
+                    .style("text-anchor", "middle")
+                    .style("font-size", "75%")
+                    .attr("y", 20)
+                    .text(function(d) {return d.name})
+            
+            node.exit().remove;
         }
 
         // left click -> fix, unfix
       
         function fixNode(d) {
-            d3.select(this).select("circle").style("stroke-width", 3);
+            console.log(d)
+            d3.select(this).select("circle").classed({'node-selected': true}).style("stroke-width", 3);
             d.fixed = true;
         }
         
@@ -181,7 +193,7 @@ var wiki = {
         
         // right click -> context menu
 
-        function showContextMenu(that, d) {
+        function showNodeContextMenu(that, d) {
             d3.event.preventDefault();
 
             var position = d3.mouse(that);
@@ -189,26 +201,26 @@ var wiki = {
 //            console.log(d3.select("svg")[0][0])
 
             var position = d3.mouse(d3.select("body")[0][0]);
-            d3.select('#context-menu')
+            d3.select('#node-context-menu')
                 .style('position', 'absolute')
                 .style('left', (position[0]+20) + "px")
                 .style('top', (position[1]+20) + "px")
                 .style('display', 'inline-block')
                 .on('mouseover', function() {
-                    d3.select('#context-menu')
+                    d3.select('#node-context-menu')
                         .style("cursor", "hand")
                 })
                 .on('mouseleave', function() {
-                    d3.select('#context-menu')
+                    d3.select('#node-context-menu')
                         .style('display', 'none')
                         .style("cursor", "arrow")
                 })
                 
-            d3.select("#context-menu")
+            d3.select("#node-context-menu")
                     .selectAll("li")
                     .on("click", function() {
                         if (this.id == "open") {
-                            d3.select('#context-menu')
+                            d3.select('#node-context-menu')
                                 .style('display', 'none')
                                 .style("cursor", "arrow")
                             
@@ -225,7 +237,7 @@ var wiki = {
                             extractNode(d)
                         }
                         
-                        d3.select("#context-menu").style('display', 'none');
+                        d3.select("#node-context-menu").style('display', 'none');
                     })
         }
 
@@ -234,7 +246,7 @@ var wiki = {
         function hideNode(node) {
             force.stop();
             
-            var originalNodes = force.nodes(); //#a
+            var originalNodes = force.nodes();
             var originalLinks = force.links();
             
             var influentialNodes = originalNodes.filter(function (d) {
@@ -244,12 +256,12 @@ var wiki = {
             var influentialLinks = originalLinks.filter(function (d) {
                 return influentialNodes.indexOf(d.source) > -1 &&
                         influentialNodes.indexOf(d.target) > -1;
-            }); //#b
+            });
             
             d3.selectAll("g.node")
                 .data(influentialNodes, function (d) {return d.id})
                 .exit()
-                .transition() //#c
+                .transition()
                 .duration(1000)
                 .style("opacity", 0)
                 .remove();
@@ -276,21 +288,9 @@ var wiki = {
         }
         
         function extractNodeCallback(nodes, edges) {
-            force.stop();
+//            force.stop();
          
-            var nodeHash = {};
-            for (x in nodes) {
-                nodeHash[nodes[x].id] = nodes[x];
-            }
-            for (x in edges) {
-                if (edges[x].type == "IS_SPOUSE_OF") {
-                    edges[x].weight = 5;
-                } else {
-                    edges[x].weight = 1;
-                }
-                edges[x].source = nodeHash[edges[x].source];
-                edges[x].target = nodeHash[edges[x].target];
-            }
+            populateEdges(nodes, edges);
             
             var oldEdges = force.links();
             var oldNodes = force.nodes();
@@ -326,7 +326,7 @@ var wiki = {
                 return true;
             })
             
-            force.links(oldEdges).nodes(oldNodes);
+//            force.links(oldEdges).nodes(oldNodes);
             
             showGraph(oldNodes, oldEdges)
             
